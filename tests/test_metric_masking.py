4047e0d4a206de5514c6ee168a97f51c6dc39619
Dieuwke Hupkes <dieuwkehupkes@gmail.com>
2016-12-20 14:34:31 +0100

    Add shape as param in tests, correct typo warning(s)

--- tests/test_metric_masking.py
+++ tests/test_metric_masking.py
@@ -10,6 +10,8 @@ from keras.engine.training import masked_metric
 from keras import metrics
 from keras import backend as K
 
+import warnings
+
 @keras_test
 def test_masking():
     np.random.seed(1337)
@@ -26,49 +28,53 @@ def test_masking():
 
 @keras_test
 def test_masking_binary_accuracy():
-    masking_function_test('binary_accuracy')
+    _masking_function_test('binary_accuracy', shape=(3,4,1))
 
 @keras_test
 def test_masking_mse():
-    masking_function_test('mse')
+    _masking_function_test('mse')
 
 @keras_test
 def test_masking_mae():
-    masking_function_test('mae')
+    _masking_function_test('mae')
 
 @keras_test
 def test_masking_mape():
-    masking_function_test('mape')
+    _masking_function_test('mape')
 
 @keras_test
 def test_masking_msle():
-    masking_function_test('msle')
+    _masking_function_test('msle')
 
 @keras_test
 def test_masking_hinge():
-    masking_function_test('hinge')
+    _masking_function_test('hinge')
 
 @keras_test
 def test_masking_squared_hinge():
-    masking_function_test('squared_hinge')
+    _masking_function_test('squared_hinge')
 
 @keras_test
 def test_masking_poisson():
-    masking_function_test('poisson')
+    _masking_function_test('poisson')
 
 
-
-def masking_function_test(fn_name):
+def _masking_function_test(fn_name, shape=(3,4,2)):
+    # test if masked_metric and original metric
+    # give the same result
     fn = metrics.get(fn_name)
     fn_masked = masked_metric(fn)
     shape = (3, 4, 2)
-    X1 = np.arange(24).reshape(shape)
-    X2 = np.concatenate((X1, np.zeros(6).reshape(3,1,2)), axis=1)
+    X1 = np.arange(np.prod(shape)).reshape(shape)
+    padding_shape = list(shape)
+    padding_shape[1] = 1
+    padding_shape = tuple(padding_shape)
+    X2 = np.concatenate((X1, np.random.rand(np.prod(padding_shape)).reshape(padding_shape)), axis=1)
 
     Y1 = 2*X1
     Y2 = 2*X2
 
-    mask = np.ones((3,5))
+    mask = np.ones((shape[0],shape[1]+1))
     mask[:,-1] = 0
 
     mse_X1_Y1         = K.eval(fn(K.variable(X1),
@@ -96,6 +102,8 @@ def masking_function_test(fn_name):
     # masked mse X2-Y2 should be equal to mse X1-Y1
     # use almost equal to account for float precision
     assert abs(mse_X1_Y1 - mse_X2_Y2_masked) < 0.0001, "Masked value not computed correctly for metric %s" % fn
+
+    assert abs(mse_X2_Y2 - mse_X1_Y1) > 0.0001, "Metric %s not sufficiently tested as masking did not result in different metric value" % fn_name
 
 
 if __name__ == '__main__':
